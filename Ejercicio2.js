c2987a149adad31671123dd5b20efab4b156bdb4
deyciandreacamargoramos5-creator <dcamargor@unsa.edu.pe>
2025-11-04 22:40:28 -0500

    Ejercicio 2

---
+++ Ejercicio2.js
@@ -0,0 +1,6 @@
+function numeroAleatorio(min, max){
+    min=Math.ceil(min);
+    max=Math.floor(max);
+    return Math.floor(Math.random()*(max-min+1)+min);
+};
+console.log(numeroAleatorio(1,10));
